7c8578e9c945df7518ada878ab4fac1f0e613a34
idbeny <>
2023-02-16 02:45:35 +0800

    fix:not find header

--- CIOAuth/CIOAuthSDK.framework/Headers/CIOAuthSDK.h
+++ CIOAuth/CIOAuthSDK.framework/Headers/CIOAuthSDK.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char CIOAuthSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CIOAuthSDK/PublicHeader.h>
 
-
+#import <CIOAuthSDK/CIOAuthTool.h>
