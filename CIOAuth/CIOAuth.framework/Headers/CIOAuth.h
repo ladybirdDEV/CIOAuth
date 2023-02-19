d2c7d5e412a5bb7ce8251ce6b7e42804417171a3
idbeny <>
2023-02-19 22:19:09 +0800

    feat:update framework

---
+++ CIOAuth/CIOAuth.framework/Headers/CIOAuth.h
@@ -0,0 +1,18 @@
+//
+//  CIOAuth.h
+//  CIOAuth
+//
+//  Created by daben on 2023/2/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CIOAuth.
+FOUNDATION_EXPORT double CIOAuthVersionNumber;
+
+//! Project version string for CIOAuth.
+FOUNDATION_EXPORT const unsigned char CIOAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIOAuth/PublicHeader.h>
+
+#import <CIOAuth/CIOAuthTool.h>
